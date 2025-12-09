b90db815e66fc123a7b237514d711f89b1d78a43
Isma <cazalla1988@gmail.com>
2025-12-09 13:12:16 +0100

    chore(config): allow images from all external domains (**) in next.config.ts to prevent crashes with third-party URLs

--- next.config.ts
+++ next.config.ts
@@ -5,47 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**.amazon.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.amazon.es',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.amazon.co.uk',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.amazon.de',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.amazon.fr',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.amazon.it',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.amazon.ca',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.amazon.co.jp',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.media-amazon.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.ssl-images-amazon.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
+        hostname: '**',
       },
     ],
   },
